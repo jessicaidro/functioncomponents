0cfaa72eeace4eaef2d49d9b62d69849f2b4830b
jessicaidro <jessicaidro@gmail.com>
2021-09-08 17:02:48 -0300

    Aula 2 - Primeiro Function Component

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FormularioCadastro(){
+    return(
+        <form>
+            <input type="text" />
+        </form>
+    );   
+
+}
+
+export default FormularioCadastro;
