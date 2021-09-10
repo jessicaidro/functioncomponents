95cc8d2139c4c3793c63d75ba319c0a304891ea6
jessicaidro <jessicaidro@gmail.com>
2021-09-10 20:34:10 -0300

    Formulário

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import Button from "@material-ui/core/Button";
 
 function FormularioCadastro(){
     return(
         <form>
+            <label>Nome</label>
             <input type="text" />
-        </form>
+    
+            <label>Sobrenome</label>
+            <input type="text" />
+    
+            <label>CPF</label>
+            <input type="text" />
+    
+            <label>Promoções</label>
+            <input type="checkbox" />
+            <label>Novidades</label>
+            <input type="checkbox" />
+    
+            <Button type="submit" variant="contained" color="primary">
+                Cadastrar
+            </Button>
+      </form>
     );   
 
 }
